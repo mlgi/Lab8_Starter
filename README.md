a21f578a2a231903eeb16da5676dc2e0e244f05c
Miguel Christian Sanchez <mcsanche@ucsd.edu>
2021-11-12 18:36:02 -0800

    init, 1 todo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Lab 8 - Starter
+1) I would put my automated tests within a github action that runs whenever code is pushed to make sure any commits don't break the branch/repo. This way, we are testing every step of the way and do not have to do it manually.
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -23,17 +23,19 @@ describe('Basic user flow for Website', () => {
     let data, plainValue;
     // Query select all of the <product-item> elements
     const prodItems = await page.$$('product-item');
-    console.log(`Checking product item 1/${prodItems.length}`);
-    // Grab the .data property of <product-items> to grab all of the json data stored inside
-    data = await prodItems[0].getProperty('data');
-    // Convert that property to JSON
-    plainValue = await data.jsonValue();
-    // Make sure the title, price, and image are populated in the JSON
-    if (plainValue.title.length == 0) { allArePopulated = false; }
-    if (plainValue.price.length == 0) { allArePopulated = false; }
-    if (plainValue.image.length == 0) { allArePopulated = false; }
-    // Expect allArePopulated to still be true
-    expect(allArePopulated).toBe(true);
+    for (let i = 0; i < prodItems.length; i++) {
+      console.log(`Checking product item ${i+1}/${prodItems.length}`);
+      // Grab the .data property of <product-items> to grab all of the json data stored inside
+      data = await prodItems[i].getProperty('data');
+      // Convert that property to JSON
+      plainValue = await data.jsonValue();
+      // Make sure the title, price, and image are populated in the JSON
+      if (plainValue.title.length == 0) { allArePopulated = false; }
+      if (plainValue.price.length == 0) { allArePopulated = false; }
+      if (plainValue.image.length == 0) { allArePopulated = false; }
+      // Expect allArePopulated to still be true
+      expect(allArePopulated).toBe(true);
+    }
 
     // TODO - Step 1
     // Right now this function is only checking the first <product-item> it found, make it so that
@@ -47,9 +49,15 @@ describe('Basic user flow for Website', () => {
     console.log('Checking the "Add to Cart" button...');
     // TODO - Step 2
     // Query a <product-item> element using puppeteer ( checkout page.$() and page.$$() in the docs )
+    const prodItems = await page.$$('product-item');
     // Grab the shadowRoot of that element (it's a property), then query a button from that shadowRoot.
+    const shRoot = await prodItems[0].getProperty('shadowRoot');
+    const button = await shRoot.$$('button');
     // Once you have the button, you can click it and check the innerText property of the button.
+    const inText = await button.click();
     // Once you have the innerText property, use innerText['_remoteObject'].value to get the text value of it
+    expect(inText['_remoteObject']).toBe('Remove from Cart');
+
   }, 2500);
 
   // Check to make sure that after clicking "Add to Cart" on every <product-item> that the Cart
